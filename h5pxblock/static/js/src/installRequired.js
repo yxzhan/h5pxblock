4545756205862409a70b3d533e3ee6f66c65db91
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-07-30 12:52:22 +0200

    studio does not support version newer than 3.5.1

--- h5pxblock/static/js/src/installRequired.js
+++ h5pxblock/static/js/src/installRequired.js
@@ -2,7 +2,7 @@ try {
   (function (require) {
     require.config({
       paths: {
-        h5p: "https://cdn.jsdelivr.net/npm/h5p-standalone@3.7.0/dist/main.bundle",
+        h5p: "https://cdn.jsdelivr.net/npm/h5p-standalone@3.5.1/dist/main.bundle",
       },
     });
   }).call(this, require || RequireJS.require);
