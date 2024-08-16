810fd6c07dc9782f1634671e6ae7f5db0bc0273c
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-08-16 14:15:20 +0200

    upgrade mathjax to 3.2.2

--- h5pxblock/public/js/h5pcustom.js
+++ h5pxblock/public/js/h5pcustom.js
@@ -1,9 +1,9 @@
 
- /**
-  * This file is executed inside iframe of h5p player so code which needs to be executed inside iframe
-  * should be places here
-  */
- getCookie = function(key, value, options) {
+/**
+ * This file is executed inside iframe of h5p player so code which needs to be executed inside iframe
+ * should be places here
+ */
+getCookie = function (key, value, options) {
 
     // key and at least value given, set cookie...
     if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
@@ -23,15 +23,15 @@
         return (document.cookie = [
             encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
             options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-            options.path    ? '; path=' + options.path : '',
-            options.domain  ? '; domain=' + options.domain : '',
-            options.secure  ? '; secure' : ''
+            options.path ? '; path=' + options.path : '',
+            options.domain ? '; domain=' + options.domain : '',
+            options.secure ? '; secure' : ''
         ].join(''));
     }
 
     // key and possibly options given, get cookie...
     options = value || {};
-    var decode = options.raw ? function(s) { return s; } : decodeURIComponent;
+    var decode = options.raw ? function (s) { return s; } : decodeURIComponent;
 
     var pairs = document.cookie.split('; ');
     for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
@@ -46,33 +46,43 @@ function csrfSafeMethod(method) {
 };
 
 H5P.jQuery.ajaxSetup({
-    beforeSend: function(xhr, settings) {
+    beforeSend: function (xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
         }
     }
 });
 
-// Add mathJax library
-H5P.externalDispatcher.on('domChanged', function(data) {
-    (function () {
-        var script = document.createElement('script');
-        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/latest.js?config=TeX-AMS-MML_HTMLorMML';
-        script.async = true;
-        document.head.appendChild(script);
-    })();
-    /***
-     * Since the interactive book only loads the first two pages in the begeinning, 
-     * need to manually call function MathJax.Hub.Typeset() after the rest of the page is loaded.
-     * Listen to the click event of the next button to call the function.
-     */
-    document.body.addEventListener('click', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        const classesOfNavBtn = ['h5p-interactive-book-status-arrow', 'navigation-button'];
-        const isNavBtn = classesOfNavBtn.some(cls => event.target.classList.contains(cls));
-        if (isNavBtn) {
-            MathJax.Hub.Typeset();
-        } 
+// Load mathJax library
+function loadMathJax(onload) {
+    var script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-svg-full.min.js';
+    script.async = true;
+    document.head.appendChild(script);
+    script.onload = onload;
+}
+
+/***
+ * Since the interactive book only loads the first two pages in the begeinning, 
+ * need to manually call function MathJax.typeset() after the rest of the page is loaded.
+ * Listen to the click event of the navigation buttons to call the typeset function.
+ */
+function pageNavCallback(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const classesOfNavBtn = [
+        '.h5p-interactive-book-navigation-chapter-button',
+        '.h5p-interactive-book-status-arrow'
+    ];
+    const isNavBtn = event.target.closest(classesOfNavBtn.join(', ')) !== null;
+    if (isNavBtn) {
+        setTimeout(MathJax.typeset, 0);
+    }
+}
+
+// Load mathjax when the H5P content is ready
+H5P.externalDispatcher.on('domChanged', function (data) {
+    loadMathJax(function () {
+        document.body.addEventListener('click', pageNavCallback);
     });
 });
